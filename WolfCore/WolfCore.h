e30d0381e0c8bdc096eaf94a1f9fb9686c985a1c
Robert McNally <robert@arciem.com>
2015-12-02 09:19:30 -0800

    Initial commit.

---
+++ WolfCore/WolfCore.h
@@ -0,0 +1,19 @@
+//
+//  WolfCore.h
+//  WolfCore
+//
+//  Created by Robert McNally on 12/2/15.
+//  Copyright © 2015 Arciem. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for WolfCore.
+FOUNDATION_EXPORT double WolfCoreVersionNumber;
+
+//! Project version string for WolfCore.
+FOUNDATION_EXPORT const unsigned char WolfCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WolfCore/PublicHeader.h>
+
+
