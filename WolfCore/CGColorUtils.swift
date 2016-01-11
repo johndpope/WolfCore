1135026b9c37c33c0b839e89c17aaaec46d8026e
Robert McNally <robert@arciem.com>
2016-01-11 07:50:17 -0800

    Adding functions from Arciem.

---
+++ WolfCore/CGColorUtils.swift
@@ -0,0 +1,194 @@
+//
+//  CGColorUtils.swift
+//  WolfCore
+//
+//  Created by Robert McNally on 1/10/16.
+//  Copyright © 2016 Arciem. All rights reserved.
+//
+
+#if os(OSX)
+    import Cocoa
+#elseif os(iOS) || os(tvOS)
+    import UIKit
+#endif
+
+import CoreGraphics
+
+public var sharedColorSpaceRGB = CGColorSpaceCreateDeviceRGB()
+public var sharedColorSpaceGray = CGColorSpaceCreateDeviceGray()
+public var sharedWhiteColor = CGColorCreate(sharedColorSpaceGray, [CGFloat(1.0), CGFloat(1.0)])
+public var sharedBlackColor = CGColorCreate(sharedColorSpaceGray, [CGFloat(0.0), CGFloat(1.0)])
+public var sharedClearColor = CGColorCreate(sharedColorSpaceGray, [CGFloat(0.0), CGFloat(0.0)])
+
+public func CGColorCreateByDarkening(color color: CGColor, fraction: CGFloat) -> CGColor! {
+    var result: CGColor? = nil
+    
+    let oldc = CGColorGetComponents(color)
+    
+    switch CGColorSpaceGetModel(CGColorGetColorSpace(color)) {
+    case .Monochrome:
+        let gray = Math.denormalize(fraction, oldc[0], 0)
+        let a = oldc[1]
+        result = CGColorCreate(sharedColorSpaceGray, [gray, a])
+    case .RGB:
+        let r = Math.denormalize(fraction, oldc[0], 0)
+        let g = Math.denormalize(fraction, oldc[1], 0)
+        let b = Math.denormalize(fraction, oldc[2], 0)
+        let a = oldc[3]
+        result = CGColorCreate(sharedColorSpaceRGB, [r, g, b, a])
+    default:
+        fatalError("unsupported color model")
+    }
+    
+    return result
+}
+
+public func CGColorCreateByLightening(color color: CGColor, fraction: CGFloat) -> CGColor! {
+    var result: CGColor? = nil
+    
+    let oldc = CGColorGetComponents(color)
+    
+    switch CGColorSpaceGetModel(CGColorGetColorSpace(color)) {
+    case .Monochrome:
+        let gray = Math.denormalize(fraction, oldc[0], 1)
+        let a = oldc[1]
+        result = CGColorCreate(sharedColorSpaceGray, [gray, a])
+    case .RGB:
+        let r = Math.denormalize(fraction, oldc[0], 1)
+        let g = Math.denormalize(fraction, oldc[1], 1)
+        let b = Math.denormalize(fraction, oldc[2], 1)
+        let a = oldc[3]
+        result = CGColorCreate(sharedColorSpaceRGB, [r, g, b, a])
+    default:
+        fatalError("unsupported color model")
+    }
+    
+    return result
+}
+
+// Identity fraction is 0.0
+public func CGColorCreateByDodging(color color: CGColor, fraction: CGFloat) -> CGColor! {
+    var result: CGColor? = nil
+    
+    let oldc = CGColorGetComponents(color)
+    
+    switch CGColorSpaceGetModel(CGColorGetColorSpace(color)) {
+    case .Monochrome:
+        let gray = Math.denormalize(fraction, oldc[0], 1)
+        let a = oldc[1]
+        result = CGColorCreate(sharedColorSpaceGray, [gray, a])
+    case .RGB:
+        let f = fmax(1.0 - fraction, 1.0e-7)
+        let r = fmin(oldc[0] / f, 1.0)
+        let g = fmin(oldc[1] / f, 1.0)
+        let b = fmin(oldc[2] / f, 1.0)
+        let a = oldc[3]
+        result = CGColorCreate(sharedColorSpaceRGB, [r, g, b, a])
+    default:
+        fatalError("unsupported color model")
+    }
+    
+    return result
+}
+
+// Identity fraction is 0.0
+public func CGColorCreateByBurning(color color: CGColor, fraction: CGFloat) -> CGColor! {
+    var result: CGColor? = nil
+    
+    let oldc = CGColorGetComponents(color)
+    
+    switch CGColorSpaceGetModel(CGColorGetColorSpace(color)) {
+    case .Monochrome:
+        let gray = Math.denormalize(fraction, oldc[0], 0)
+        let a = oldc[1]
+        result = CGColorCreate(sharedColorSpaceGray, [gray, a])
+    case .RGB:
+        let f = fmax(1.0 - fraction, 1.0e-7)
+        let r = fmin(1.0 - (1.0 - oldc[0]) / f, 1.0)
+        let g = fmin(1.0 - (1.0 - oldc[1]) / f, 1.0)
+        let b = fmin(1.0 - (1.0 - oldc[2]) / f, 1.0)
+        let a = oldc[3]
+        result = CGColorCreate(sharedColorSpaceRGB, [r, g, b, a])
+    default:
+        fatalError("unsupported color model")
+    }
+    
+    return result
+}
+
+public func CGColorCreateByInterpolating(color1 color1: CGColor, color2: CGColor, fraction: CGFloat) -> CGColor! {
+    var result: CGColor? = nil
+    
+    let oldc1 = CGColorGetComponents(color1)
+    let oldc2 = CGColorGetComponents(color2)
+    
+    let colorSpaceModel1 = CGColorSpaceGetModel(CGColorGetColorSpace(color1))
+    let colorSpaceModel2 = CGColorSpaceGetModel(CGColorGetColorSpace(color2))
+    
+    if colorSpaceModel1 == colorSpaceModel2 {
+        switch colorSpaceModel1 {
+        case .Monochrome:
+            let gray = Math.interpolate(fraction, oldc1[0], oldc2[0])
+            let a = oldc1[1]
+            result = CGColorCreate(sharedColorSpaceGray, [gray, a])
+        case .RGB:
+            let r = Math.interpolate(fraction, oldc1[0], oldc2[0])
+            let g = Math.interpolate(fraction, oldc1[1], oldc2[1])
+            let b = Math.interpolate(fraction, oldc1[2], oldc2[2])
+            let a = oldc1[3]
+            result = CGColorCreate(sharedColorSpaceRGB, [r, g, b, a])
+        default:
+            fatalError("unsupported color model")
+        }
+    } else {
+        fatalError("color space mismatch")
+    }
+    
+    return result
+}
+
+public func CGColorCreate(r r: CGFloat, g: CGFloat, b: CGFloat, a: CGFloat) -> CGColor! {
+    return CGColorCreate(sharedColorSpaceRGB, [r, g, b, a])
+}
+
+public func CGColorCreate(gray gray: CGFloat, a: CGFloat) -> CGColor! {
+    return CGColorCreate(sharedColorSpaceGray, [gray, a])
+}
+
+public func CGColorCreateRandom(random: Random = Random.sharedInstance) -> CGColor! {
+    var components = Array(count: 4, repeatedValue: CGFloat(0))
+    for var i = 0; i < 3; ++i {
+        components[i] = random.randomCGFloat()
+    }
+    components[3] = 1
+    return CGColorCreate(sharedColorSpaceRGB, components)
+}
+
+public func CGColorConvertToRGB(color: CGColor) -> CGColor! {
+    var result: CGColor! = color
+    
+    let oldc = CGColorGetComponents(color)
+    
+    switch CGColorSpaceGetModel(CGColorGetColorSpace(color)) {
+    case .Monochrome:
+        let gray = oldc[0]
+        let a = oldc[1]
+        result = CGColorCreate(sharedColorSpaceRGB, [gray, gray, gray, a])
+    case .RGB:
+        break
+    default:
+        fatalError("unsupported color model")
+    }
+    
+    return result
+}
+
+public func CGGradientWithColors(colorFracs:[ColorFrac]) -> CGGradient {
+    var cgColors = [CGColor]()
+    var locations = [CGFloat]()
+    for colorFrac in colorFracs {
+        cgColors.append(colorFrac.color.cgColor)
+        locations.append(CGFloat(colorFrac.frac))
+    }
+    return CGGradientCreateWithColors(sharedColorSpaceRGB, cgColors, locations)!
+}
---
+++ WolfCore/Color.swift
@@ -0,0 +1,157 @@
+//
+//  Color.swift
+//  WolfCore
+//
+//  Created by Robert McNally on 1/10/16.
+//  Copyright © 2016 Arciem. All rights reserved.
+//
+
+public struct Color {
+    public let red: Float
+    public let green: Float
+    public let blue: Float
+    public let alpha: Float
+    
+    public init(red: Float, green: Float, blue: Float, alpha: Float = 1.0) {
+        self.red = red
+        self.green = green
+        self.blue = blue
+        self.alpha = alpha
+    }
+    
+    public init(redByte: Byte, greenByte: Byte, blueByte: Byte, alphaByte: Byte = 255) {
+        self.init(red: Float(redByte) / 255.0,
+            green: Float(greenByte) / 255.0,
+            blue: Float(blueByte) / 255.0,
+            alpha: Float(alphaByte) / 255.0
+        )
+    }
+    
+    public init(bytes: Bytes) {
+        let redByte = bytes[0]
+        let greenByte = bytes[1]
+        let blueByte = bytes[2]
+        var alphaByte: UInt8 = 255
+        if bytes.count >= 4 {
+            alphaByte = bytes[3]
+        }
+        self.init(redByte: redByte, greenByte: greenByte, blueByte: blueByte, alphaByte: alphaByte)
+    }
+    
+    public init(color: Color, alpha: Float) {
+        self.red = color.red
+        self.green = color.green
+        self.blue = color.blue
+        self.alpha = alpha
+    }
+    
+    public init(var hue h: Float, var saturation s: Float, var brightness v: Float, alpha a: Float = 1.0) {
+        v = Math.clamp(v, 0.0...1.0)
+        s = Math.clamp(s, 0.0...1.0)
+        alpha = a
+        if(s <= 0.0) {
+            red = v
+            green = v
+            blue = v
+        } else {
+            h %= 1.0
+            if h < 0.0 { h += 1.0 }
+            h *= 6.0
+            let i = Int(floorf(h))
+            let f = h - Float(i)
+            let p = v * (1.0 - s)
+            let q = v * (1.0 - (s * f))
+            let t = v * (1.0 - (s * (1.0 - f)))
+            switch(i) {
+            case 0: red = v; green = t; blue = p
+            case 1: red = q; green = v; blue = p
+            case 2: red = p; green = v; blue = t
+            case 3: red = p; green = q; blue = v
+            case 4: red = t; green = p; blue = v
+            case 5: red = v; green = p; blue = q
+            default: red = 0; green = 0; blue = 0; assert(false, "unknown hue sector")
+            }
+        }
+    }
+    
+    public static func randomColor(random: Random = Random.sharedInstance, alpha: Float = 1.0) -> Color {
+        return Color(
+            red: random.randomFloat(),
+            green: random.randomFloat(),
+            blue: random.randomFloat(),
+            alpha: alpha
+        )
+    }
+    
+    public func multipliedBy(🅡: Float) -> Color {
+        return Color(red: red * 🅡, green: green * 🅡, blue: blue * 🅡, alpha: alpha * 🅡)
+    }
+    
+    public func addedTo(🅡: Color) -> Color {
+        return Color(red: red + 🅡.red, green: green + 🅡.green, blue: blue + 🅡.blue, alpha: alpha + 🅡.alpha)
+    }
+    
+    public func lightened(frac: Float) -> Color {
+        return Color(
+            red: Math.denormalize(frac, red, Float(1)),
+            green: Math.denormalize(frac, green, Float(1)),
+            blue: Math.denormalize(frac, blue, Float(1)),
+            alpha: alpha)
+    }
+    
+    public func darkened(frac: Float) -> Color {
+        return Color(
+            red: Math.denormalize(frac, red, Float(0)),
+            green: Math.denormalize(frac, green, Float(0)),
+            blue: Math.denormalize(frac, blue, Float(0)),
+            alpha: alpha)
+    }
+    
+    public static let Black = Color(red: 0, green: 0, blue: 0)
+    public static let DarkGray = Color(red: 1 / 3.0, green: 1 / 3.0, blue: 1 / 3.0)
+    public static let LightGray = Color(red: 2 / 3.0, green: 2 / 3.0, blue: 2 / 3.0)
+    public static let White = Color(red: 1, green: 1, blue: 1)
+    public static let Gray = Color(red: 0.5, green: 0.5, blue: 0.5)
+    public static let Red = Color(red: 1, green: 0, blue: 0)
+    public static let Green = Color(red: 0, green: 1, blue: 0)
+    public static let DarkGreen = Color(red: 0, green: 0.5, blue: 0)
+    public static let Blue = Color(red: 0, green: 0, blue: 1)
+    public static let Cyan = Color(red: 0, green: 1, blue: 1)
+    public static let Yellow = Color(red: 1, green: 1, blue: 0)
+    public static let Magenta = Color(red: 1, green: 0, blue: 1)
+    public static let Orange = Color(red: 1, green: 0.5, blue: 0)
+    public static let Purple = Color(red: 0.5, green: 0, blue: 0.5)
+    public static let Brown = Color(red: 0.6, green: 0.4, blue: 0.2)
+    public static let Clear = Color(red: 0, green: 0, blue: 0, alpha: 0)
+    
+    public static let Chartreuse = blend(.Yellow, .Green, frac: 0.5)
+    public static let Gold = Color(redByte: 251, greenByte: 212, blueByte: 55)
+    public static let BlueGreen = Color(redByte: 0, greenByte: 169, blueByte: 149)
+    public static let MediumBlue = Color(redByte: 0, greenByte: 110, blueByte: 185)
+    public static let DeepBlue = Color(redByte: 60, greenByte: 55, blueByte: 149)
+    
+}
+
+#if os(OSX) || os(iOS) || os(tvOS)
+extension Color {
+    public var cgColor: CGColor {
+        return CGColorCreate(sharedColorSpaceRGB, [CGFloat(red), CGFloat(green), CGFloat(blue), CGFloat(alpha)])!
+    }
+}
+#endif
+
+extension Color : CustomStringConvertible {
+    public var description: String {
+        get {
+            return "Color(red:\(red) green:\(green) blue:\(blue) alpha:\(alpha))"
+        }
+    }
+}
+
+public func *(🅛: Color, 🅡: Float) -> Color {
+    return 🅛.multipliedBy(🅡)
+}
+
+public func +(🅛: Color, 🅡: Color) -> Color {
+    return 🅛.addedTo(🅡)
+}
