f74134b922614aa46d3ff76c3655dcc3d4e70cc4
Robert McNally <robert@arciem.com>
2016-01-21 00:09:55 -0800

    SHA256 for iOS.

--- WolfCore/Sources/SHA256.swift
+++ WolfCore/Sources/SHA256.swift
@@ -9,17 +9,29 @@
 import CommonCrypto
 
 public class SHA256 {
-    private(set) var digest = Bytes(count: 32, repeatedValue: 0)
+    private(set) var digest = Bytes(count: Int(CC_SHA256_DIGEST_LENGTH), repeatedValue: 0)
     
-    public init(var bytes: Bytes) {
-        CC_SHA256(&bytes, CC_LONG(bytes.count), &digest)
+    public init(message: Bytes) {
+        CC_SHA256(message, CC_LONG(message.count), &digest)
     }
-    
-//    func test() {
-//        let data = NSData()
-//        var hash = Bytes(count: 32, repeatedValue: 0)
-//        CC_SHA256(data.bytes, CC_LONG(data.length), &hash)
-//        var res : NSData = NSData.dataWithBytes(hash, length: Int(CC_SHA256_DIGEST_LENGTH))
-//    }
 }
 
+extension SHA256: CustomStringConvertible {
+    public var description: String {
+        var s = ""
+        for byte in digest {
+            s += String(byte, radix: 16)
+        }
+        return s
+    }
+}
+
+public func testSHA256() {
+    // $ openssl dgst -sha256 -hex
+    // The quick brown fox\n^d
+    // 35fb7cc2337d10d618a1bad35c7a9e957c213f00d0ed32f2454b2a99a971c0d8
+    let message = "The quick brown fox\n".bytes
+    let sha256 = SHA256(message: message)
+    print(sha256)
+    // prints 35fb7cc2337d10d618a1bad35c7a9e957c213f0d0ed32f2454b2a99a971c0d8
+}
