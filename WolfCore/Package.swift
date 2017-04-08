eb1dab63261084ae78d134e94f804d8ee7c85adb
Wolf McNally <robert5@arciem.com>
2017-04-08 00:09:13 -0700

    Linux.

--- WolfCore/Package.swift
+++ WolfCore/Package.swift
@@ -8,6 +8,10 @@ let package = Package(
 package.dependencies.append(.Package(url: "https://github.com/venj/CommonCrypto.git", versions: Version(0,3,0) ..< Version(1,0,0)))
 #endif
 
+#if os(Linux)
+package.dependencies.append(url: "https://github.com/Zewo/COpenSSL.git", majorVersion: 0, minor: 1)
+#endif
+
 //    dependencies: [
 //        //.Package(url: "https://github.com/PureSwift/CUUID.git", majorVersion: 1),
 //        //.Package(url: "https://github.com/Zewo/COpenSSL.git", majorVersion: 0, minor: 1),
