1ba6ec9e56fe4709a7c055253412f6ff9712249b
vfxdrummer <timothy.brandt@gmail.com>
2017-05-08 00:22:36 -0700

    Add buttonTitleCaps

--- WolfCore/Source-UI/FontStyle.swift
+++ WolfCore/Source-UI/FontStyle.swift
@@ -39,6 +39,7 @@ extension FontStyleName {
     public static let navbarTitle = FontStyleName("navbarTitle")
     public static let barbuttonTitle = FontStyleName("barbuttonTitle")
     public static let buttonTitle = FontStyleName("buttonTitle")
+    public static let buttonTitleCaps = FontStyleName("buttonTitleCaps")
 
     public static let textFieldContent = FontStyleName("textFieldContent")
     public static let textFieldPlaceholder = FontStyleName("textFieldPlaceholder")
